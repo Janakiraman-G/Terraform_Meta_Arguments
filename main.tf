c74b67fdb62733294cdebc774aa4a587c776c4bd
Janakiraman-G <janakiraman160603@gmail.com>
2026-09-05 20:34:18 +0530

    Third changes

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ resource "aws_s3_bucket" "example-1" {
   count  = 2
   bucket = var.bucket_name[count.index]
 
+# added tags to the S3 bucket
 
   tags = var.tags
 }
 
 # Going to create AWS S3 bucket with versioning enabled using for_each
-
 resource "aws_s3_bucket" "example-2" {
   for_each = var.bucket_name_set
   bucket   = each.key
